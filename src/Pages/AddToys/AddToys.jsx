6b5ee44160837fbbe39b9cf4c0dd4db5cab54e3e
asifrahi0 <asifahemmedrahi0@gmail.com>
2023-05-23 15:35:00 +0600

    add a toys page created

--- src/Pages/AddToys/AddToys.jsx
+++ src/Pages/AddToys/AddToys.jsx
@@ -8,15 +8,97 @@ const AddToys = () => {
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => console.log(data);
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName")} />
-      <select {...register("gender")}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">other</option>
-      </select>
-      <input type="submit" />
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo URL</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="url"
+                  className="input input-bordered"
+                  {...register("url")}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="input input-bordered"
+                  {...register("name")}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Sub Category</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Sub Category"
+                  className="input input-bordered"
+                  {...register("category")}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Price</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Price"
+                  className="input input-bordered"
+                  {...register("price")}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Ratings</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Ratings"
+                  className="input input-bordered"
+                  {...register("ratings")}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Quantity</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Quantity"
+                  className="input input-bordered"
+                  {...register("quantity")}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Detail Description</span>
+                </label>
+                <textarea
+                  className="textarea textarea-bordered h-24"
+                  placeholder="Description"
+                ></textarea>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-active btn-primary">
+                  <input type="submit" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </form>
   );
 };
